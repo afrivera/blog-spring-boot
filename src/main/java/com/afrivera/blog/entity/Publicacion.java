82de92110b37a4801f87ce3e0600ca5e7ea1c7ea
afrivera <andresrfelip@hotmail.com>
2022-10-02 21:31:12 -0500

    creación de packages, entidad publicación y configuración de la conexión en las properties del proyecto

---
+++ src/main/java/com/afrivera/blog/entity/Publicacion.java
@@ -0,0 +1,65 @@
+package com.afrivera.blog.entity;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "publicaciones", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})})
+public class Publicacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    public Publicacion() {
+    }
+
+    public Publicacion(Long id, String title, String description, String content) {
+        super();
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
